077afa6886c754dfb4875b9953a6a874249e4372
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2012-10-13 00:14:19 +0000

    first try with 6bar signal setting
    imitated from @codefireX and probably not going to work
    
    Change-Id: If1112b5af949179135c8341ed38f00f9419fe364

--- src/com/android/settings/PizzabeanSettings.java
+++ src/com/android/settings/PizzabeanSettings.java
@@ -2,21 +2,24 @@ package com.android.settings;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
-import android.util.Log;
 import android.preference.CheckBoxPreference;
-import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-
-
+import android.util.Log;
 import com.android.settings.R;
 import com.android.settings.SettingsFragment;
 
 public class PizzabeanSettings extends SettingsFragment {
 
     private final static String TAG = PizzabeanSettings.class.getSimpleName();
+    private static final String STATUSBAR_SIXBAR_SIGNAL = "pref_statusbar_sixbar_signal";
+
+    private ContentResolver mCr;
+    private PreferenceScreen mPrefSet;
+
+    private CheckBoxPreference mUseSixbaricons;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,31 @@ public class PizzabeanSettings extends SettingsFragment {
 
         addPreferencesFromResource(R.xml.pizzabean_settings);
 
+        mUseSixbaricons = (CheckBoxPreference) findPreference("statusbar_sixbar");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        mUseSixbaricons.setChecked(Settings.System.getInt(getContentResolver(),
+        		Settings.System.STATUSBAR_6BAR_SIGNAL, 0) != 0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, 
+    		Preference preference) {
+        if (preference == mUseSixbaricons) {
+            Settings.System.putInt(getContentResolver(),
+            		Settings.System.STATUSBAR_6BAR_SIGNAL,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
