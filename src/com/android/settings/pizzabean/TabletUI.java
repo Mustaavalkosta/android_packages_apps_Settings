76f65d9f398a25265694d576d2623d67f2ff73e2
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2012-10-30 11:28:10 +0000

    add LCD Density Changer adapted from @AOKP/packages_apps_ROMControl
    Change-Id: I9019cb717a64bff86aa57fe5dc321b16fc126648

--- src/com/android/settings/pizzabean/TabletUI.java
+++ src/com/android/settings/pizzabean/TabletUI.java
@@ -2,8 +2,10 @@ package com.android.settings.pizzabean;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import com.android.settings.R;
@@ -15,6 +17,8 @@ public class TabletUI extends SettingsPreferenceFragment {
 	private static final String PREF_FORCE_TABLET_UI = "force_tabletui";
     
 	private CheckBoxPreference mTabletui;
+	private Preference mLcdDensity;
+	private int newDensityValue;
     private ContentResolver mCr;
 
 	@Override
@@ -30,6 +34,16 @@ public class TabletUI extends SettingsPreferenceFragment {
 		mTabletui = (CheckBoxPreference) prefSet.findPreference(PREF_FORCE_TABLET_UI);
 		mTabletui.setChecked(Settings.System.getInt(mCr,
 				Settings.System.FORCE_TABLET_UI, 0) == 1);
+		
+		/* Density Changer */
+		mLcdDensity = findPreference("lcd_density_setup");
+		String currentProperty = SystemProperties.get("ro.sf.lcd_density");
+		try {
+			newDensityValue = Integer.parseInt(currentProperty);
+		} catch (Exception e) {
+			getPreferenceScreen().removePreference(mLcdDensity);
+		}
+		mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + currentProperty);
 	}
 	
     @Override
@@ -39,6 +53,9 @@ public class TabletUI extends SettingsPreferenceFragment {
     	if (preference == mTabletui) {
     		Settings.System.putInt(mCr, Settings.System.FORCE_TABLET_UI,
     				((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+    		return true;
+    	} else if (preference == mLcdDensity) {
+    		((PreferenceActivity) getActivity()).startPreferenceFragment(new DensityChanger(), true);
     		return true;
     	}
         
